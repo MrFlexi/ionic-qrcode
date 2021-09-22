ccf6493872e8dd9c7a2f3496452d2039372cfe7a
MrFlexi <szaroletta@hotmail.com>
2021-09-22 16:00:15 +0200

    Barcode

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Injectable } from '@angular/core';
 import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
 import { BarcodeScanner } from '@ionic-native/barcode-scanner';
-
+import { Geolocation } from '@capacitor/geolocation';
 
 @Injectable({
   providedIn: 'root'
@@ -11,25 +11,37 @@ import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 
 export class PhotoService {
   
+  latitude: any = 22; //latitude
+  longitude: any = 33; //longitude
+  barcode: any = 'Hallo Welt';
+
+  constructor() {
+    this.getLocation();
+  }
 
   public async addNewToGallery() {
     // Take a photo
-    
     BarcodeScanner.scan().then(barcodeData => {
       console.log('Barcode data', barcodeData);
+      this.barcode = barcodeData;
      }).catch(err => {
          console.log('Error', err);
-     });    
-   
-  }
+     });
+    }
 
   public async takePhoto() {
-    // Take a photo    
+    // Take a photo
     const capturedPhoto = await Camera.getPhoto({
       resultType: CameraResultType.Uri,
       source: CameraSource.Camera,
       quality: 100
     });
   }
-  
+
+    public async getLocation() {
+      const position = await Geolocation.getCurrentPosition();
+      this.latitude = position.coords.latitude;
+      this.longitude = position.coords.longitude;
+    }
+
 }
